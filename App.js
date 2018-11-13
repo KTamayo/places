c3c3a71d6c95500928c0996c9317b1c0fe079429
Karlo Tamayo <karlo.tamayo.1010@gmail.com>
2018-11-12 22:29:13 -0800

    adds reducer logic and dispatcher hooks

--- App.js
+++ App.js
@@ -4,10 +4,17 @@ import {
   View,
 } from 'react-native';
 
+import { connect } from 'react-redux';
+import {
+  addPlace,
+  deletePlace,
+  selectPlace,
+  deselectPlace,
+} from './src/store/actions/index';
+
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
 import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
-import placeImage from './src/assets/tj_v_roque.jpg';
 
 const styles = StyleSheet.create({
   container: {
@@ -19,70 +26,81 @@ const styles = StyleSheet.create({
   },
 });
 
-export default class App extends Component {
-  state = {
-    places: [],
-    selectedPlace: null,
-  }
+class App extends Component {
+  
 
   placeAddedHandler = (placeName) => {
-    this.setState((prevState) => {
-      if (Object.values(prevState.places).includes(placeName)) {
-        alert(`${placeName} is already in use!`);
-        return;
-      }
-      return {
-        places: prevState.places.concat({
-          key: Math.random(),
-          name: placeName,
-          image: placeImage,
-        }),
-      };
-    });
+    // this.setState((prevState) => {
+    // if (Object.values(this.props.places).includes(placeName)) {
+    //   alert(`${placeName} is already in use!`);
+    //   return;
+    // }
+    this.props.onAddPlace(placeName);
   }
 
-  placeSelectedHandler = (key) => {
-    this.setState((prevState) => {
-      return {
-        selectedPlace: prevState.places.find( (place) => {
-          return place.key === key;
-        }),
-      };
-    });
-    
-  };
-
   placeDeletedHandler = () => {
-    this.setState((prevState) => {
-      return {
-        places: prevState.places.filter((place) => {
-          return place.key !== prevState.selectedPlace.key;
-        }),
-        selectedPlace: null,
-      };
-    });
+    this.props.onDeletePlace();
+    // this.setState((prevState) => {
+    //   return {
+    //     places: prevState.places.filter((place) => {
+    //       return place.key !== prevState.selectedPlace.key;
+    //     }),
+    //     selectedPlace: null,
+    //   };
+    // });
   };
 
-  modalClosedHandler = () => {
-    this.setState({
-      selectedPlace: null,
-    });
+  placeSelectedHandler = (key) => {
+    this.props.onSelectPlace(key);
+    // this.setState((prevState) => {
+    //   return {
+    //     selectedPlace: prevState.places.find( (place) => {
+    //       return place.key === key;
+    //     }),
+    //   };
+    // });    
+  };
+
+
+  modalClosedHandler = () => {    
+    this.props.onDeselectPlace();
+    // this.setState({
+      // selectedPlace: null,
+    // });
   }
 
   render() {
     return (
       <View style={styles.container}>
         <PlaceDetail
-        selectedPlace={this.state.selectedPlace}
-        onItemDeleted={this.placeDeletedHandler}
-        onModalClosed={this.modalClosedHandler}
-      />
+          selectedPlace={this.props.selectedPlace}
+          onItemDeleted={this.placeDeletedHandler}
+          onModalClosed={this.modalClosedHandler}
+        />
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
         <PlaceList
-          places={this.state.places}
+          places={this.props.places}
           onItemSelected={this.placeSelectedHandler}
         />
       </View>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    places: state.places.places,
+    selectedPlace: state.places.selectedPlace,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAddPlace: name => dispatch(addPlace(name)),
+    onDeletePlace: () => dispatch(deletePlace()),
+    onSelectPlace: key => dispatch(selectPlace(key)),
+    onDeselectPlace: () => dispatch(deselectPlace()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -5,6 +5,8 @@ import {
   DESELECT_PLACE,
 } from '../actions/actionTypes';
 
+import placeImage from '../../assets/tj_v_roque.jpg';
+
 const initialState = {
   places: [],
   selectedPlace: null,
@@ -12,6 +14,39 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_PLACE:
+      // if (Object.values(state.places).includes(action.placeName)) {      
+      //   alert(`${placeName} is already in use!`);
+      //   return;
+      // }
+      return {
+        ...state,
+        places: state.places.concat({
+          key: Math.random(),
+          name: action.placeName,
+          image: placeImage,
+        }),
+      };              
+    case DELETE_PLACE:
+      return {
+        ...state,
+        places: state.places.filter((place) => {
+          return place.key !== state.selectedPlace.key;
+        }),
+        selectedPlace: null,
+      };
+    case SELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: state.places.find((place) => {
+          return place.key === action.placeKey;
+        }),
+      };
+    case DESELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: null,
+      };
     default:
       return state;
   }
